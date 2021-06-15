e9b0dcf302a399755d2f151a9fa2be80df4cda0d
Jayesh Narwaria <36211228+jaynarw@users.noreply.github.com>
2021-06-16 01:47:54 +0530

    add base packages

---
+++ src/components/CalendarContainer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./styles.module.css";
+import "./index.css";
+
+export interface CalendarContainerProps {}
+
+export const CalendarContainer: React.FC<CalendarContainerProps> = () => {
+  return <div></div>;
+};
