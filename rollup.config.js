61e68e67031cf97083a54928f4a62a7ab3be8fcd
Jayesh Narwaria <36211228+jaynarw@users.noreply.github.com>
2021-06-22 22:19:46 +0530

    v0.1.4

--- rollup.config.js
+++ rollup.config.js
@@ -6,33 +6,39 @@ import postcss from "rollup-plugin-postcss";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import { terser } from "rollup-plugin-terser";
+import dts from "rollup-plugin-dts";
 // import copy from "rollup-plugin-copy";
 
 const packageJson = require("./package.json");
 
-export default {
-    input: "src/lib/index.ts",
-    output: [{
-            file: packageJson.main,
-            format: "cjs",
-            sourcemap: true,
-        },
-        {
+export default [{
+        input: "src/lib/index.ts",
+        output: [{
             file: packageJson.module,
             format: "esm",
             sourcemap: true,
-        },
-    ],
-    plugins: [
-        peerDepsExternal(),
-        resolve(),
-        commonjs(),
-        postcss({
-            extract: false,
-            modules: true,
-            plugins: [autoprefixer(), cssnano()],
-        }),
-        typescript({ useTsconfigDeclarationDir: true }),
-        terser(),
-    ],
-};
+        }, ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            postcss({
+                extract: false,
+                modules: true,
+                plugins: [autoprefixer(), cssnano()],
+            }),
+            typescript({
+                useTsconfigDeclarationDir: true,
+                rollupCommonJSResolveHack: false,
+                clean: true,
+            }),
+            // dts(),
+            terser(),
+        ],
+    },
+    {
+        input: "./src/lib/index.d.ts",
+        output: [{ file: "build/index.d.ts", format: "es" }],
+        plugins: [dts()],
+    },
+];
